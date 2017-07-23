66b68fa5bac84eb14c7e266e2b2b39436114b2af
win1010437630 <1010437630@qq.com>
2017-07-23 20:24:17 +0800

    20:25

--- src/Live.js
+++ src/Live.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './Live.css';
 import { Carousel } from 'antd';
+import $ from 'jquery';
 
 class Live extends Component {
   componentDidMount(){
-
+	$('.slick-prev').text("<");
+	$('.slick-next').text(">");
   }
   render() {
     return (
@@ -27,59 +29,49 @@ class Live extends Component {
 				<div><h3>3</h3></div>
 				<div><h3>4</h3></div>
 			</Carousel>
-        	<div className='left live_banner'>
-        		<div className='banner_first'>
-        			<a className='location'></a>
-        			<ul className='slides'>
-        				<li></li>
-        			</ul>
-		        	<div className='lbtn btn'>&lt;</div>
-		        	<div className='rbtn btn'>&gt;</div>
-        		</div>    		
-        	</div>
         </div>
         <div className='live_banner_second clear'>
-        	<div className='left banner_left'>
-        		<a className='location'></a>
-        		<ul className='focus'></ul>
-        		<div className='lbtn btn'>&lt;</div>
-        		<div className='rbtn btn'>&gt;</div>
-        	</div>
-        	<div className='right banner_left'>
-        		<a className='location'></a>
-        		<ul className='focus'></ul>
-        		<div className='lbtn btn'>&lt;</div>
-        		<div className='rbtn btn'>&gt;</div>
-        	</div>
+        	<Carousel dots="false" arrows="true">
+				<div><h3>1</h3></div>
+				<div><h3>2</h3></div>
+				<div><h3>3</h3></div>
+				<div><h3>4</h3></div>
+			</Carousel>
+			<Carousel dots="false" arrows="true">
+				<div><h3>1</h3></div>
+				<div><h3>2</h3></div>
+				<div><h3>3</h3></div>
+				<div><h3>4</h3></div>
+			</Carousel>
         </div>
         <div className='live_banner_third clear'>
-        	<div className='left banner_one'>
-        		<a className='location'></a>
-        		<ul className='focus'></ul>
-        		<div className='lbtn btn'>&lt;</div>
-        		<div className='rbtn btn'>&gt;</div>
-        	</div>
-        	<div className='left banner_one'>
-        		<a className='location'></a>
-        		<ul className='focus'></ul>
-        		<div className='lbtn btn'>&lt;</div>
-        		<div className='rbtn btn'>&gt;</div>
-        	</div>
-        	<div className='right banner_one'>
-        		<a className='location'></a>
-        		<ul className='focus'></ul>
-        		<div className='lbtn btn'>&lt;</div>
-        		<div className='rbtn btn'>&gt;</div>
-        	</div>
+			<Carousel dots="false" arrows="true">
+				<div><h3>1</h3></div>
+				<div><h3>2</h3></div>
+				<div><h3>3</h3></div>
+				<div><h3>4</h3></div>
+			</Carousel>
+			<Carousel dots="false" arrows="true">
+				<div><h3>1</h3></div>
+				<div><h3>2</h3></div>
+				<div><h3>3</h3></div>
+				<div><h3>4</h3></div>
+			</Carousel>
+			<Carousel dots="false" arrows="true">
+				<div><h3>1</h3></div>
+				<div><h3>2</h3></div>
+				<div><h3>3</h3></div>
+				<div><h3>4</h3></div>
+			</Carousel>
         </div>
         
         <div className='live_banner_second clear'>
-        	<div className='left banner_left'>
-        		<a className='location'></a>
-        		<ul className='focus'></ul>
-        		<div className='lbtn btn'>&lt;</div>
-        		<div className='rbtn btn'>&gt;</div>
-        	</div>
+        	<Carousel dots="false" arrows="true">
+				<div><h3>1</h3></div>
+				<div><h3>2</h3></div>
+				<div><h3>3</h3></div>
+				<div><h3>4</h3></div>
+			</Carousel>
         	<div className='right banner_right'>
         		<a className='location'></a>
         		<ul className='focus'></ul>
